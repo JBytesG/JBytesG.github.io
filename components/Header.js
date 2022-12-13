7a545d63c5f80f9ce14090fb57348cf102e8f152
Oggeus <joebgorton@gmail.com>
2022-12-13 12:14:59 -0600

    Initial work

---
+++ components/Header.js
@@ -0,0 +1,32 @@
+class Header extends HTMLElement {
+    constructor() {
+        super();
+    }
+
+    connectedCallback() {
+        this.innerHTML = `
+        <header>
+            <h1>
+                Senior Portfolio
+            </h1>
+        </header>
+        <nav>
+            <ul>
+                <li><a href="../index.html">Home</a></li>
+                <li>
+                    <div class="dropdown">
+                        <a href="../reflections">Reflections</a>
+                        <div class="dropdown_content">
+                            <a href="../highSchoolReflection.html">High School</a>
+                        </div>
+                    </div>
+                </li>
+                <li><a href="../artifacts.html">Artifacts</a></li>
+                <li><a href="../letters.html">Letters of Recomendation</a></li>
+            </ul>
+        </nav>
+        `;
+    }
+}
+
+customElements.define("header-and-nav", Header);
